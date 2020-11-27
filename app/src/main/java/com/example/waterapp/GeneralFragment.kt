2431a09189521f1d72b77dd6f04d27010ab54114
TorisonRa <torisonra@ya.ru>
2020-11-27 19:14:38 +0900

    L-01 Верстка general_fragment.xml Добавил строки в strings.xml; Создал dimen.xml с переменными размерогв шрифтов и отступов; Создал styles_components.xml c стилем кнопки; В themes.xml отключил шторку с названием приложения; Добавил в colors.xml переменные основных цветов приложения; Загрузил картинку стакана.

--- app/src/main/java/com/example/waterapp/GeneralFragment.kt
+++ app/src/main/java/com/example/waterapp/GeneralFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 
-class GeneralFragment:Fragment() {
+class GeneralFragment : Fragment(R.layout.general_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        SampleApplication.INSTANCE.router.navigateTo()
+//       SampleApplication.INSTANCE.router.navigateTo()
     }
 }
